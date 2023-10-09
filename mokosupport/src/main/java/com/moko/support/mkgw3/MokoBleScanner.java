f41c1541102c4f463552d36544319a7018ddc13f
jun.liu <812515547@qq.com>
2023-10-09 11:38:43 +0800

    升级项目目标版本和编译版本到33

--- mokosupport/src/main/java/com/moko/support/mkgw3/MokoBleScanner.java
+++ mokosupport/src/main/java/com/moko/support/mkgw3/MokoBleScanner.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.core.content.ContextCompat;
+
 import no.nordicsemi.android.support.v18.scanner.BluetoothLeScannerCompat;
 import no.nordicsemi.android.support.v18.scanner.ScanCallback;
 import no.nordicsemi.android.support.v18.scanner.ScanFilter;
@@ -36,9 +37,7 @@ public final class MokoBleScanner {
 
     public void startScanDevice(MokoScanDeviceCallback callback) {
         mMokoScanDeviceCallback = callback;
-        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            XLog.i("Start scan");
-        }
+        XLog.i("Start scan");
         final BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
         ScanSettings settings = new ScanSettings.Builder()
                 .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
@@ -55,9 +54,7 @@ public final class MokoBleScanner {
 
     public void stopScanDevice() {
         if (mMokoLeScanHandler != null && mMokoScanDeviceCallback != null) {
-            if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                XLog.i("End scan");
-            }
+            XLog.i("End scan");
             final BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
             scanner.stopScan(mMokoLeScanHandler);
             mMokoScanDeviceCallback.onStopScan();
